dbf804ead93345e71106eedebb8c256638ae9b65
Egorka_fggg <znobiseve@gmail.com>
2025-08-27 12:50:58 +0300

    основное задание

--- src/main/java/org/studyss/Main.java
+++ src/main/java/org/studyss/Main.java
@@ -1,7 +1,38 @@
 package org.studyss;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int comprand() {
+        return (int) (Math.random() * 100);
+    }
+
     public static void main(String[] args) {
 
+        int compnumber = comprand();
+        int numperson;
+        int res;
+        boolean sswitch=true;
+        int attempts=0;
+
+        Scanner sc = new Scanner(System.in);
+        while (sswitch) {
+            System.out.println("Число загадано, что это за число?");
+            numperson = sc.nextInt();
+            if (compnumber == numperson) {
+                System.out.println("Вы угадали!!!ТЫ красава!");
+                sswitch = false;
+
+            } else if (compnumber > numperson) {
+                System.out.println("Я сам в шоке, но загаданное число больше, брат");
+                attempts++;
+
+            } else if (compnumber < numperson) {
+                System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат");
+                attempts++;
+            }
+
         }
+        System.out.println("Количество попыток: "+attempts+" Загаданное число: "+compnumber);
     }
+}
